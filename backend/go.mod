e842b4a210292fef8890157b576771ce35ba48cc
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-03-28 13:20:07 -0400

    fix: clean up code

--- backend/go.mod
+++ backend/go.mod
@@ -3,7 +3,8 @@ module good_exchange
 go 1.20
 
 require (
-	firebase.google.com/go/v4 v4.10.0
+	cloud.google.com/go/firestore v1.9.0
+	firebase.google.com/go v3.13.0+incompatible
 	github.com/gin-gonic/gin v1.9.0
 	google.golang.org/api v0.114.0
 )
@@ -12,12 +13,9 @@ require (
 	cloud.google.com/go v0.110.0 // indirect
 	cloud.google.com/go/compute v1.18.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/firestore v1.9.0 // indirect
 	cloud.google.com/go/iam v0.12.0 // indirect
 	cloud.google.com/go/longrunning v0.4.1 // indirect
 	cloud.google.com/go/storage v1.28.1 // indirect
-	firebase.google.com/go v3.13.0+incompatible // indirect
-	github.com/MicahParks/keyfunc v1.5.1 // indirect
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -25,13 +23,13 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
-	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -52,7 +50,6 @@ require (
 	golang.org/x/time v0.1.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/appengine/v2 v2.0.2 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
 	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.29.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,102 +1,109 @@
 package main
-//importing
-import (
-	"context"
-	// "fmt"
-	"net/http"
-	"os"
-	"log"
-//intall go get firebase.google.com/go/v4  go get github.com/gin-gonic/gin
 
-	"firebase.google.com/go"
+import (
 	"cloud.google.com/go/firestore"
+	"context"
+	"firebase.google.com/go"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
+	"log"
+	"net/http"
 )
-//take proprety from json  
+
+const SERVICE_ACCOUNT_FILENAME = "ics4uo-project-firebase-key.json"
+
+var firebaseContext context.Context
+var firebaseApp *firebase.App
+var firestoreClient *firestore.Client
+
 type Donation struct {
-	ID          string `json:"id"`
-	Title		string `json:"title"`
-	Description string `json:"description"`
-	Location    string `json:"location"`
-	ImgSrc    	string `json:"imgsrc"`
+	ID          string   `json:"id"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Location    string   `json:"location"`
+	Images      []string `json:"imgs"`
 }
 
+func getDonationsListEndpoint(c *gin.Context) {
+	donations, err := getAllDonations(firebaseContext, firestoreClient)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	} else {
+		c.IndentedJSON(http.StatusOK, donations)
+	}
+}
+
+func getDonationFromIDEndpoint(c *gin.Context) {
+	id := c.Param("id")
+
+	donation, err := getDonationByID(firebaseContext, firestoreClient, id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"error": err.Error()})
+	} else {
+		c.IndentedJSON(http.StatusOK, donation)
+	}
+}
 
 func main() {
-	
-	ctx :=context.Background()
-	//allow go code to interact with firebase safetly
-	serviceAccountKeyFile:="ics4uo-project-firebase-key.json"
-	opt := option.WithCredentialsFile(serviceAccountKeyFile)
-	//create new firebase app with options
-	app, err := firebase.NewApp(ctx, nil, opt)
+	firebaseContext = context.Background()
+	firebaseCreds := option.WithCredentialsFile(SERVICE_ACCOUNT_FILENAME)
+
+	app, err := firebase.NewApp(firebaseContext, nil, firebaseCreds)
 	if err != nil {
 		log.Fatalf("Error initializing Firebase app: %v\n", err)
-		os.Exit(1)
 	}
-		// Initialize firestore client
-	client, err := app.Firestore(ctx)
+	firebaseApp = app
+
+	firestoreClient, err = firebaseApp.Firestore(firebaseContext)
 	if err != nil {
 		log.Fatalf("Error initializing Firestore client: %v\n", err)
-		os.Exit(1)
 	}
 
-	// Initialize gin
 	r := gin.Default()
-	
-	//retrieves donations from getAllDonations() ([]Donation,error) and sends a JSON response to the fronend with a http statusOK and a response body as a donations slice
-	r.GET("/donations/donationList",func(c *gin.Context) { 
-		donations,err :=getAllDonations(ctx,client)
-		if err != nil{
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		} else {
-			c.JSON(http.StatusOK,donations)
-		}
-	})
-	r.GET("/donations/:id",func(c *gin.Context){
-		id := c.Param("id")
-		
-		donation,err := getDonationByID(ctx,client,id)
-		if err!=nil{
-			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-		}else{
-			c.JSON(http.StatusOK,donation)
-		}
 
-	})
-	
-	r.Run(":4000")
+	r.GET("/donations/donationList", getDonationsListEndpoint)
+	r.GET("/donations/:id", getDonationFromIDEndpoint)
+
+	err = r.Run()
+	if err != nil {
+		return
+	}
 }
-//go function can return 2 things, in this case it is a donation struct and the error
-func getAllDonations(ctx context.Context,client *firestore.Client) ([]Donation, error) {
+
+// Gets all donations available
+func getAllDonations(ctx context.Context, client *firestore.Client) ([]Donation, error) {
 	var donations []Donation
-	iter :=client.Collection("donations").Documents(ctx) //.Documents(ctx) returns a iterator
+	iter := client.Collection("donations").Documents(ctx) //.Documents(ctx) returns a iterator
 	for {
-		doc,err:=iter.Next()
-		if err==iterator.Done{
+		doc, err := iter.Next()
+		if err == iterator.Done {
 			break
 		}
-		if err!=nil {
-			return nil,err //no data was retrieved-nil, but there was an error -err
+		if err != nil {
+			return nil, err //no data was retrieved-nil, but there was an error -err
 		}
 		var donation Donation
-		doc.DataTo(&donation)
-		donation.ID=doc.Ref.ID //sets donation struct id to the one in the firebase
-		donations=append(donations,donation)
+		err = doc.DataTo(&donation)
+		if err != nil {
+			return nil, err
+		}
+		donation.ID = doc.Ref.ID //sets donation struct id to the one in the firebase
+		donations = append(donations, donation)
 	}
-	return donations,nil //nil-data was retrived without any errors
+	return donations, nil //nil-data was retrived without any errors
 }
 
-func getDonationByID(ctx context.Context, client *firestore.Client,id string) (Donation,error) {
+func getDonationByID(ctx context.Context, client *firestore.Client, id string) (Donation, error) {
 	var donation Donation
-	doc,err:=client.Collection("donations").Doc(id).Get(ctx) //get a single donation from its id
-	if err!=nil {
-		return donation,err //returns empty donation struct
+	doc, err := client.Collection("donations").Doc(id).Get(ctx) //get a single donation from its id
+	if err != nil {
+		return donation, err //returns empty donation struct
 	}
-	doc.DataTo(&donation)
-	donation.ID=doc.Ref.ID
-	return donation,nil
+	err = doc.DataTo(&donation)
+	if err != nil {
+		return Donation{}, err
+	}
+	donation.ID = doc.Ref.ID
+	return donation, nil
 }
-
