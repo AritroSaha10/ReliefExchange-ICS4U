098b1313ded523cd80c7cfd533e386990448fa68
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-05-19 00:22:43 -0400

    fix(backend): remove random prints

--- backend/main.go
+++ backend/main.go
@@ -396,7 +396,6 @@ func reportDonation(ctx context.Context, client *firestore.Client, donationID st
 	}
 
 	// Check whether they have already reported this donation
-	log.Println(doc.Data()["reports"].([]interface{}))
 	currentReports, ok := doc.Data()["reports"].([]string)
 	if !ok {
 		// Convert the empty interface types to actual strings
@@ -408,7 +407,6 @@ func reportDonation(ctx context.Context, client *firestore.Client, donationID st
 
 	// Check whether they've already made a report
 	for _, report := range currentReports {
-		log.Println(report)
 		if report == userUID {
 			return errors.New("User has already sent a report")
 		}
