e7cf1a34f99831e11b8cefc4f70b7e45c33e45b7
Joshua Chou <126603520+JoshuaChou10@users.noreply.github.com>
2023-05-23 14:38:57 -0400

    Tried to make test not interact directly with base

--- backend/go.mod
+++ backend/go.mod
@@ -16,9 +16,9 @@ require (
 
 require (
 	cloud.google.com/go v0.110.0 // indirect
-	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute v1.19.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/iam v0.12.0 // indirect
+	cloud.google.com/go/iam v0.13.0 // indirect
 	cloud.google.com/go/longrunning v0.4.1 // indirect
 	cloud.google.com/go/storage v1.28.1 // indirect
 	github.com/bytedance/sonic v1.8.0 // indirect
@@ -30,7 +30,7 @@ require (
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
@@ -61,8 +61,8 @@ require (
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
-	google.golang.org/grpc v1.53.0 // indirect
-	google.golang.org/protobuf v1.29.1 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/grpc v1.54.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- backend/main_test.go
+++ backend/main_test.go
@@ -15,7 +15,12 @@ import (
 
 	"cloud.google.com/go/firestore"
 	"github.com/getsentry/sentry-go"
+	ptypes "github.com/golang/protobuf/ptypes"
 	_ "github.com/joho/godotenv/autoload"
+
+	mockfs "github.com/weathersource/go-mockfs"
+
+	pb "google.golang.org/genproto/googleapis/firestore/v1"
 )
 
 func TestMain(m *testing.M) {
@@ -26,12 +31,40 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatalf("Error initializing Sentry: %s", err)
 	}
+	var t *testing.T
+	client, server, err := mockfs.New()
+	assert.NotNil(t, client)
+	assert.NotNil(t, server)
+	assert.Nil(t, err)
 
 	// Initialize Firebase globals
-	err = globals.InitializeFirebaseGlobals()
-	if err != nil {
-		return
-	}
+	// Populate a mock document "b" in collection "C"
+	var (
+		aTime          = time.Date(2017, 1, 26, 0, 0, 0, 0, time.UTC)
+		aTime2         = time.Date(2017, 2, 5, 0, 0, 0, 0, time.UTC)
+		aTimestamp, _  = ptypes.TimestampProto(aTime)
+		aTimestamp2, _ = ptypes.TimestampProto(aTime2)
+		dbPath         = "projects/projectID/databases/(default)"
+		path           = "projects/projectID/databases/(default)/documents/C/b"
+		pdoc           = &pb.Document{
+			Name:       path,
+			CreateTime: aTimestamp,
+			UpdateTime: aTimestamp,
+			Fields:     map[string]*pb.Value{"f": {ValueType: &pb.Value_IntegerValue{int64(1)}}},
+		}
+	)
+	server.AddRPC(
+		&pb.BatchGetDocumentsRequest{
+			Database:  dbPath,
+			Documents: []string{path},
+		},
+		[]interface{}{
+			&pb.BatchGetDocumentsResponse{
+				Result:   &pb.BatchGetDocumentsResponse_Found{pdoc},
+				ReadTime: aTimestamp2,
+			},
+		},
+	)
 	os.Exit(m.Run())
 }
 
@@ -53,7 +86,7 @@ func TestAddDonation(t *testing.T) {
 		Tags:              []string{"tag1", "tag2"},
 		Reports:           []string{"report1", "report2"},
 	}
-	test_user_id := "4P9lIlcIYNeeCZja6Wc3coemX1A3" //Joshua.C
+	test_user_id := "p48oQ0SAYPeqculMRp2UBNJl03d2" //Joshua.C
 	donationId, err := helpers.AddDonation(donation, test_user_id)
 	assert.NoError(t, err, "addDonation function should return without error")
 	assert.NotEmpty(t, donationId, "addDonation should return a donation id ")
@@ -76,7 +109,7 @@ func TestAddDonation(t *testing.T) {
 }
 
 func TestGetDonationById(t *testing.T) {
-	test_user_id := "4P9lIlcIYNeeCZja6Wc3coemX1A3" //Joshua.C
+	test_user_id := "p48oQ0SAYPeqculMRp2UBNJl03d2" //Joshua.C
 	owner, err := globals.FirestoreClient.Doc("users/" + test_user_id).Get(globals.FirebaseContext)
 	assert.NoError(t, err, "Owner should have been retrieved properly")
 
@@ -98,8 +131,8 @@ func TestGetDonationById(t *testing.T) {
 }
 
 func TestCheckIfAdmin(t *testing.T) {
-	test_user_id := "4P9lIlcIYNeeCZja6Wc3coemX1A3" //Joshua.C
+	test_user_id := "p48oQ0SAYPeqculMRp2UBNJl03d2" //Joshua.C
 	isAdmin, err := helpers.CheckIfAdmin(test_user_id)
 	assert.NoError(t, err, "GetDonationById function should return without error")
-	assert.False(t, isAdmin, "Joshua.C is not an admin")
+	assert.True(t, isAdmin, "Joshua.C is an admin")
 }
