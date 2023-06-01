9b6608e62fb06e722ff3c8d4f251d035155d66b8
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-06-01 19:52:51 -0400

    fix(backend): closes #63

--- backend/endpoints/post/add_donation.go
+++ backend/endpoints/post/add_donation.go
@@ -25,13 +25,14 @@ Available: https://pkg.go.dev/github.com/reiver/go-iter. [Accessed: 30- May- 202
 package post
 
 import (
-    "net/http"
-    "relief_exchange_backend/globals"
-    "relief_exchange_backend/helpers"
-    "relief_exchange_backend/types"
+	"net/http"
+	"relief_exchange_backend/globals"
+	"relief_exchange_backend/helpers"
+	"relief_exchange_backend/types"
 
-    "github.com/gin-gonic/gin"
-    log "github.com/sirupsen/logrus"
+	"cloud.google.com/go/firestore"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 // postDonationEndpoint handles the endpoint to post a new donation.
@@ -41,37 +42,59 @@ import (
 // It accepts a donation and a user's id token, verifies the token,
 // and then uses the addDonation function to add the donation to the database.
 func AddDonation(c *gin.Context) {
-    var body struct {
-        DonationData types.Donation `json:"data"`
-        IDToken      string         `json:"token"`
-    }
-    // Bind the request body to the body struct, this stores the donation data and id token of the user to allow go to use.
-    if err := c.ShouldBindJSON(&body); err != nil {
-        log.Error(err.Error())
-        c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
-    // Verify the IdToken of the sender (user) with the server
-    token, err := globals.AuthClient.VerifyIDToken(globals.FirebaseContext, body.IDToken)
-    if err != nil {
-        log.Warn("Failed to verify ID token")
-        c.IndentedJSON(http.StatusForbidden, gin.H{"error": "You are not authorized to make this donation."})
-        return
-    }
+	var body struct {
+		DonationData types.Donation `json:"data"`
+		IDToken      string         `json:"token"`
+	}
+	// Bind the request body to the body struct, this stores the donation data and id token of the user to allow go to use.
+	if err := c.ShouldBindJSON(&body); err != nil {
+		log.Error(err.Error())
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// Verify the IdToken of the sender (user) with the server
+	token, err := globals.AuthClient.VerifyIDToken(globals.FirebaseContext, body.IDToken)
+	if err != nil {
+		log.Warn("Failed to verify ID token")
+		c.IndentedJSON(http.StatusForbidden, gin.H{"error": "You are not authorized to make this donation."})
+		return
+	}
 
-    userUID := token.UID
+	userUID := token.UID
 
-    // Use addDonation function to add the donation, passing in the donationData and the uid
-    docID, err := helpers.AddDonation(body.DonationData, userUID)
+	// Use addDonation function to add the donation, passing in the donationData and the uid
+	docID, err := helpers.AddDonation(body.DonationData, userUID)
 
-    // If there's an error adding the donation, send back err msg to frontend,
-    // otherwise send back docId for the frontend to use
-    if err != nil {
-        log.Error(err.Error())
-        c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    } else {
-        log.Info("Post donation successful.")
-        c.IndentedJSON(http.StatusCreated, docID)
-    }
+	// If there's an error adding the donation, send back err msg to frontend,
+	// otherwise send back docId for the frontend to use
+	if err != nil {
+		log.Error(err.Error())
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	} else {
+		log.Info("Post donation successful.")
+		c.IndentedJSON(http.StatusCreated, docID)
+	}
+
+	// Increment the user's donation counter.
+	// Run this after setting up the response since this isn't a priority
+	// and won't affect it.
+	userData, err := helpers.GetUserDataByID(token.UID)
+	// Only do it if there was no error
+	// Don't bother returning an error to endpoint since updating the counter
+	// isn't a proper failure
+	if err == nil {
+		// Update user data with new donations count
+		_, err = globals.FirestoreClient.Collection("users").Doc(token.UID).Update(globals.FirebaseContext, []firestore.Update{
+			{
+				Path:  "donations_made",
+				Value: userData.DonationsMade + 1,
+			},
+		})
+	}
+
+	// Log any errors that occured
+	if err != nil {
+		log.Error(err.Error())
+	}
 }
